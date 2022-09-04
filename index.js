5bed97acf1e04055289a70fdd7140fd85a9758e2
shoaibahmadsakib <shoaibahmadsakib@gmail.com>
2022-09-04 10:10:18 +0600

    new

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const dataBase = require('./utils/db')
 const app = express()
-const port = 3000
+const port = 3000 || process.env.PORT
 const cors = require('cors')
 const userRouter = require('./router/v1/User.router')
 
